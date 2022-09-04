41a976db319631fc06ad89d96df85ec76a69f9d3
Philipp Fuerholz <fuerholz@gmx.ch>
2022-09-04 22:54:43 +0200

    added 1MHz timer

---
+++ Src/stm32f446/timer.c
@@ -0,0 +1,22 @@
+#include "timer.h"
+#include "stm32f446/stm32f446xx.h"
+
+// timer 5 as a 1MHz/us timer
+
+void initTimer()
+{
+    RCC->APB1ENR |= (1 << RCC_APB1ENR_TIM5EN_Pos);
+    TIM5->PSC=90-1;
+    TIM5->CNT=0;
+    TIM5->CR1=(1 << TIM_CR1_CEN_Pos);    
+}
+
+void resetTime()
+{
+    TIM5->CNT=0;
+}
+
+uint32_t getTimeLW()
+{
+    return TIM5->CNT;
+}
