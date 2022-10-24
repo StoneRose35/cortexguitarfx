412dbe5273b8c085479a5573e6f3ce8643578008
Philipp Fuerholz <fuerholz@gmx.ch>
2022-10-24 22:15:21 +0200

    adc/pots fixed

--- Src/stm32f446/adc.c
+++ Src/stm32f446/adc.c
@@ -10,15 +10,9 @@ volatile uint16_t adcChannelValues[3];
 
 void DMA2_Stream0_IRQHandler() //raised when all 3 adc values have been read
 {
-    DMA2_Stream0->M0AR=(uint32_t)adcChannelValues;
-    DMA2_Stream0->NDTR = 3;
-    DMA2->LIFCR = (1 << DMA_LIFCR_CTCIF0_Pos); //| (1 << DMA_LIFCR_CHTIF0_Pos);
-    ADC1->SR |= (1 << ADC_SR_EOC_Pos);
-    ADC1->CR2 &= ~(1 << ADC_CR2_DMA_Pos);
-    ADC1->CR2 |= (1 << ADC_CR2_DMA_Pos);
-    DMA2_Stream0->CR |= (1 << DMA_SxCR_EN_Pos);
+    TIM2->CR1 &= ~(1 << TIM_CR1_CEN_Pos);
+    DMA2->LIFCR = (1 << DMA_LIFCR_CTCIF0_Pos);
     task |= (1 << TASK_UPDATE_POTENTIOMETER_VALUES);
-
 }
 
 /**
@@ -52,8 +46,8 @@ void initAdc()
     uint32_t port;
 
     RCC->APB2ENR |= (1 << RCC_APB2ENR_ADC1EN_Pos);
-    ADC->CCR |= (3 << ADC_CCR_ADCPRE_Pos); // divide by 8 --> 90Mzh/8= 1.333 MSPS
-    ADC1->SQR1 |= (3 << ADC_SQR1_L_Pos);
+    ADC->CCR |= (3 << ADC_CCR_ADCPRE_Pos); // divide by 8 --> 90Mzh/8= 13.33 MSPS
+    ADC1->SQR1 |= (2 << ADC_SQR1_L_Pos);
     ADC1->SQR3 = (POT1_CHANNEL << 0) | (POT2_CHANNEL << 5) | (POT3_CHANNEL << 10);
     setSampleCycles(2,POT1_CHANNEL);
     setSampleCycles(2,POT2_CHANNEL);
@@ -77,36 +71,51 @@ void initAdc()
     port = POT3 >> 4;
     RCC->AHB1ENR |= (1 << port);
     gpio=(GPIO_TypeDef*)(GPIOA_BASE + port*0x400);
-    gpio->MODER |= (3 << ((POT2 & 0xF)<<1));
-    gpio->PUPDR &= ~(3 << ((POT2 & 0xF)<< 1));
+    gpio->MODER |= (3 << ((POT3 & 0xF)<<1));
+    gpio->PUPDR &= ~(3 << ((POT3 & 0xF)<< 1));
+
+        //setup timer 2
+    RCC->APB1ENR |= (1 << RCC_APB1ENR_TIM2EN_Pos);
+    //TIM2->PSC = 65217; // divide 90MHz by 65217 (Prescaler) and 46 (ARR, CCR2) to achieve 30Hz ADC Sampling Frequency
+                       // among all three channels thus update the potentiometers at 10Hz
+    //TIM2->ARR=46;
+    //TIM2->CCR2=46;
+    TIM2->PSC = 65535;
+    TIM2->ARR=137;
+    TIM2->CCR2=137;
 }
 
 void initRoundRobinReading()
 {
-    // use Timer 2 CC2 as a Trigger source
-    ADC1->CR2 |= (1 << ADC_CR2_CONT_Pos) | (1 << ADC_CR2_ADON_Pos) | (1 << ADC_CR2_EXTEN_Pos) | (3 << ADC_CR2_EXTSEL_Pos)
-                 | (1 << ADC_CR2_DMA_Pos) | (1 << ADC_CR2_EOCS_Pos); // | (1 << ADC_CR2_DDS_Pos);
-    
-    //setup timer 2
-    RCC->APB1ENR |= (1 << RCC_APB1ENR_TIM2EN_Pos);
-    TIM2->PSC = 65217; // divide 90MHz by 65217 (Prescaler) and 46 (ARR, CCR2) to achieve 30Hz ADC Sampling Frequency
-                       // among all three channels thus update the potentiometers at 10Hz
-    TIM2->ARR=46;
-    TIM2->CCR2=46;
     TIM2->CNT=0;
+    TIM2->CCMR1 = (3 << TIM_CCMR1_OC2M_Pos);
+    TIM2->CCER |= (1 << TIM_CCER_CC2E_Pos);
     TIM2->CR1 |= (1 << TIM_CR1_CEN_Pos);
 
     // setup dma 2 channel 0 stream 0
     DMA2_Stream0->PAR=(uint32_t)&(ADC1->DR);
     DMA2_Stream0->M0AR=(uint32_t)adcChannelValues;
     DMA2_Stream0->M1AR=(uint32_t)adcChannelValues;
-    DMA2_Stream0->CR = (1 << DMA_SxCR_MSIZE_Pos) | (1 << DMA_SxCR_PSIZE_Pos) | (1 << DMA_SxCR_MINC_Pos) | (1 << DMA_SxCR_TCIE_Pos);
+    DMA2_Stream0->CR = (1 << DMA_SxCR_MSIZE_Pos) | (1 << DMA_SxCR_PSIZE_Pos) | (1 << DMA_SxCR_MINC_Pos) | (1 << DMA_SxCR_TCIE_Pos) | (1 << DMA_SxCR_CIRC_Pos);
     DMA2_Stream0->NDTR=3;
     DMA2_Stream0->CR |=(1 << DMA_SxCR_EN_Pos);
     NVIC_EnableIRQ(DMA2_Stream0_IRQn);
 
-    // start adc conversion
-    ADC1->CR2 |= (1 << ADC_CR2_SWSTART_Pos);
+    // use Timer 2 CC2 as a Trigger source
+    ADC1->CR2 |= (1 << ADC_CR2_ADON_Pos) | (1 << ADC_CR2_EXTEN_Pos) | (3 << ADC_CR2_EXTSEL_Pos) | (0 << ADC_CR2_CONT_Pos) | (0 << ADC_CR2_EOCS_Pos)
+                 | (1 << ADC_CR2_DMA_Pos) | (1 << ADC_CR2_DDS_Pos);
+}
+
+void restartAdc()
+{
+    //DMA2_Stream0->M0AR=(uint32_t)adcChannelValues;
+    //DMA2_Stream0->NDTR = 3;
+    //DMA2_Stream0->CR |= (1 << DMA_SxCR_EN_Pos);
+    //ADC1->CR2 &= ~(1 << ADC_CR2_DMA_Pos);
+    //ADC1->CR2 |= (1 << ADC_CR2_DMA_Pos);
+    TIM2->SR &= ~(1 << TIM_SR_CC2IF_Pos);
+    TIM2->CNT=0;
+    TIM2->CR1 |= (1 << TIM_CR1_CEN_Pos);
 }
 
 uint16_t getChannel0Value()
