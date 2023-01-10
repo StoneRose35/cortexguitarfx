3f7c099066090f59cc76aca7acd18162fa8928d2
Philipp Fuerholz <fuerholz@gmx.ch>
2023-01-10 22:30:31 +0100

    changed readme file in usb ramdrive

--- Src/rp2040/usb_msc_ramdrive_impl.c
+++ Src/rp2040/usb_msc_ramdrive_impl.c
@@ -4,25 +4,28 @@
 
 
 #define README_CONTENTS \
-"This is tinyusb's MassStorage Class demo.\r\n\r\n\
-If you find any bugs or get any questions, feel free to file an\r\n\
-issue at github.com/hathach/tinyusb"
+"This is the demo readme file for the PiPicoFX Guitar FX device.\r\n\
+You probably access this file when attaching a PiPicoFX to a computer over USB\r\n\
+or if you're browsing through the code at https://github.com/StoneRose35/cortexguitarfx.\r\n\
+you can edit this file, however it will not be persisted when turning of the device.\
+"
 
 uint8_t msc_disk[TOTAL_SIZE_IN_BLOCKS][LOGICAL_BLOCK_SIZE] =
 {
+  // Taken from TinyUSB MSC CDC example
   //------------- Block0: Boot Sector -------------//
   // byte_per_sector    = DISK_BLOCK_SIZE; fat12_sector_num_16  = DISK_BLOCK_NUM;
   // sector_per_cluster = 1; reserved_sectors = 1;
   // fat_num            = 1; fat12_root_entry_num = 16;
   // sector_per_fat     = 1; sector_per_track = 1; head_num = 1; hidden_sectors = 0;
   // drive_number       = 0x80; media_type = 0xf8; extended_boot_signature = 0x29;
-  // filesystem_type    = "FAT12   "; volume_serial_number = 0x1234; volume_label = "TinyUSB MSC";
+  // filesystem_type    = "FAT12   "; volume_serial_number = 0x1234; volume_label = "PiPicoFX MSC";
   // FAT magic code at offset 510-511
   {
       0xEB, 0x3C, 0x90, 0x4D, 0x53, 0x44, 0x4F, 0x53, 0x35, 0x2E, 0x30, 0x00, 0x02, 0x01, 0x01, 0x00,
       0x01, 0x10, 0x00, 0x10, 0x00, 0xF8, 0x01, 0x00, 0x01, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00,
-      0x00, 0x00, 0x00, 0x00, 0x80, 0x00, 0x29, 0x34, 0x12, 0x00, 0x00, 'T' , 'i' , 'n' , 'y' , 'U' ,
-      'S' , 'B' , ' ' , 'M' , 'S' , 'C' , 0x46, 0x41, 0x54, 0x31, 0x32, 0x20, 0x20, 0x20, 0x00, 0x00,
+      0x00, 0x00, 0x00, 0x00, 0x80, 0x00, 0x29, 0x34, 0x12, 0x00, 0x00, 'P' , 'i' , 'P' , 'i' , 'c' ,
+      'o' , 'F' , 'X' , ' ' , ' ' , ' ' , 0x46, 0x41, 0x54, 0x31, 0x32, 0x20, 0x20, 0x20, 0x00, 0x00,
 
       // Zero up to 2 last bytes of FAT magic code
       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
@@ -66,12 +69,12 @@ uint8_t msc_disk[TOTAL_SIZE_IN_BLOCKS][LOGICAL_BLOCK_SIZE] =
   //------------- Block2: Root Directory -------------//
   {
       // first entry is volume label
-      'T' , 'i' , 'n' , 'y' , 'U' , 'S' , 'B' , ' ' , 'M' , 'S' , 'C' , 0x08, 0x00, 0x00, 0x00, 0x00,
+      'P' , 'i' , 'P' , 'i' , 'c' , 'o' , 'F' , 'X' , ' ' , ' ' , ' ' , 0x08, 0x00, 0x00, 0x00, 0x00,
       0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x4F, 0x6D, 0x65, 0x43, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
       // second entry is readme file
       'R' , 'E' , 'A' , 'D' , 'M' , 'E' , ' ' , ' ' , 'T' , 'X' , 'T' , 0x20, 0x00, 0xC6, 0x52, 0x6D,
       0x65, 0x43, 0x65, 0x43, 0x00, 0x00, 0x88, 0x6D, 0x65, 0x43, 0x02, 0x00,
-      sizeof(README_CONTENTS)-1, 0x00, 0x00, 0x00 // readme's files size (4 Bytes)
+      (sizeof(README_CONTENTS)-1) & 0xFF, (sizeof(README_CONTENTS)-1)>>8, 0x00, 0x00 // readme's files size (4 Bytes)
   },
 
   //------------- Block3: Readme Content -------------//
