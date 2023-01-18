9d8f6e11a3a61c56285a3794c8e6c550febb601b
Philipp Fuerholz <fuerholz@gmx.ch>
2023-01-18 22:41:23 +0100

    added play functionality to offline processor

--- Tests/offlineProcessor.py
+++ Tests/offlineProcessor.py
@@ -4,6 +4,10 @@ from tkinter import ttk, font
 import os
 import subprocess
 import json
+import pyaudio
+import wave
+import threading
+import time
 
 
 class OfflineProcessorGui:
@@ -16,30 +20,51 @@ class OfflineProcessorGui:
         self.w.option_add("*Font", bigfont)
         self.w.title("PiPicoFX Offline Processor")
         self.w.geometry("760x1000")
+        self.w["bg"] = "#9bb0d1"
 
+        self.w.grid_columnconfigure(0, weight=1)
+        self.w.grid_columnconfigure(1, weight=1)
+        self.w.grid_columnconfigure(2, weight=1)
         # look for all audio samples and put them in a combobox
-        cbSamples = ttk.Combobox(self.w, width=38)
-        sampleVals = []
+        self.cbSamples = ttk.Combobox(self.w)
+        sample_vals = []
         for el in os.listdir("./audiosamples"):
             if not el.endswith("_proc.wav"):
                 dname = el.replace(".wav", "")
-                sampleVals.append(dname)
-        cbSamples["values"] = sampleVals
-        cbSamples.grid(pady=10, padx=10, sticky="w")
+                sample_vals.append(dname)
+        self.cbSamples["values"] = sample_vals
+        self.cbSamples.grid(pady=10, padx=10, sticky="ew", columnspan=3)
 
         # list all fx programs and put them into a combobox
-        fxPrograms = call_backed(["-o"])
-        cbPrograms = ttk.Combobox(self.w, width=38)
-        cbPrograms["values"] = fxPrograms["programs"]
-        cbPrograms.grid(pady=10, padx=10, sticky="w")
-        cbPrograms.bind("<<ComboboxSelected>>", self.fxprogram_changed)
-
+        fx_programs = call_backed(["-o"])
+        cb_programs = ttk.Combobox(self.w)
+        cb_programs["values"] = fx_programs["programs"]
+        cb_programs.grid(pady=10, padx=10, sticky="ew", columnspan=3)
+        cb_programs.bind("<<ComboboxSelected>>", self.fxprogram_changed)
+        self.program_values = []
         self.param_frame = tk.Frame(self.w)
-        self.param_frame.columnconfigure(0,weight=1, minsize=120)
-        self.param_frame.columnconfigure(1,weight=3)
-        self.param_frame.grid(sticky="w")
+        self.param_frame.columnconfigure(0, weight=1, minsize=120)
+        self.param_frame.columnconfigure(1, weight=3)
+        self.param_frame.grid(sticky="ew", columnspan=3)
+        self.param_frame["bg"] = "#bacce8"
+
+        self.btnProcessSample = tk.Button(self.w, text="Process & Play", command=self.process_sample)
+        self.btnProcessSample.grid(column=0, row=3, columnspan=1, sticky="ew", padx=15, pady=15)
+
+        self.btnStopSample = tk.Button(self.w, text="Stop Audio", command=self.stop_audio)
+        self.btnStopSample.grid(column=1, row=3, columnspan=1, sticky="ew", padx=15, pady=15)
+        self.p = pyaudio.PyAudio()
+        self.audioplayer_thread = threading.Thread(target=self.play_wav_file)
+        self.audio_playing = False
 
         self.w.mainloop()
+
+        # stop audio if playing
+        if self.audioplayer_thread.is_alive():
+            self.audio_playing = False
+            while self.audioplayer_thread.is_alive():
+                time.sleep(0.01)
+        self.p.terminate()
 
     def fxprogram_changed(self, event):
 
@@ -49,37 +74,75 @@ class OfflineProcessorGui:
         # clear slider and add new ones
         for s in self.sliders:
             s.grid_remove()
-        for l in self.labels:
-            l.grid_remove()
+        for label in self.labels:
+            label.grid_remove()
         self.sliders = []
         self.labels = []
         cnt = 0
-        self.programValues = []
+
         for pn in params["parameterNames"]:
-            lbl = tk.Label(self.param_frame, text=pn["name"],width=20,justify="left",anchor="w")
-            lbl.grid(column=0, row=cnt,sticky="sw",padx=10)
+            lbl = tk.Label(self.param_frame, text=pn["name"], width=20, justify="left", anchor="w")
+            lbl["bg"] = "#bacce8"
+            lbl.grid(column=0, row=cnt, sticky="sw", padx=10)
             self.labels.append(lbl)
-            self.programValues.append(tk.StringVar(self.param_frame))
-            val = tk.Label(self.param_frame, text=" ", textvariable=self.programValues[-1])
-            val.grid(column=0, row=cnt+1,sticky="nw",padx=10)
+            self.program_values.append(tk.StringVar(self.param_frame))
+            val = tk.Label(self.param_frame, text=" ", textvariable=self.program_values[-1])
+            val["bg"] = "#bacce8"
+            val.grid(column=0, row=cnt+1, sticky="nw", padx=10)
             self.labels.append(val)
-            scl = tk.Scale(self.param_frame, orient="horizontal", from_=0, to=4095, command=self.param_changed, width=30, length=400)
-            scl.grid(column=1, row=cnt, rowspan=2,pady=16)
+            scl = tk.Scale(self.param_frame, orient="horizontal", from_=0, to=4095,
+                           command=self.param_changed, width=30, length=400)
+            scl.grid(column=1, row=cnt, rowspan=2, pady=16)
+            scl["bg"] = "#bacce8"
             self.sliders.append(scl)
 
-            cnt +=2
+            cnt += 2
         pass
 
-    def param_changed(self,event):
-        args=["-v",str(self.currentProgram) ]
+    def param_changed(self):
+        args = ["-v", str(self.currentProgram)]
         for s in self.sliders:
             args.append(str(int(s.get())))
-        currentVals = call_backed(args)
-        for v in zip(currentVals["parameterValues"],self.programValues):
+        current_vals = call_backed(args)
+        for v in zip(current_vals["parameterValues"], self.program_values):
             v[1].set(v[0]["value"])
-
-
         pass
+
+    def process_sample(self):
+        samplename = "./audiosamples/" + self.cbSamples["values"][self.cbSamples.current()] + ".wav"
+        samplename_proc = "./audiosamples/" + self.cbSamples["values"][self.cbSamples.current()] + "_proc.wav"
+        if os.path.exists(samplename_proc):
+            os.remove(samplename_proc)
+        args = ["-p", samplename, str(self.currentProgram)]
+        for s in self.sliders:
+            args.append(str(int(s.get())))
+        call_backed(args)
+        if os.path.exists(samplename_proc):
+            if self.audioplayer_thread.is_alive():
+                self.audio_playing = False
+                while self.audioplayer_thread.is_alive():
+                    time.sleep(0.01)
+
+            self.audioplayer_thread = threading.Thread(target=self.play_wav_file, args=[samplename_proc])
+            self.audioplayer_thread.start()
+
+    def stop_audio(self):
+        self.audio_playing = False
+
+    def play_wav_file(self, wavfile):
+        wf = wave.open(wavfile, "rb")
+        self.audio_playing = True
+        stream = self.p.open(format=self.p.get_format_from_width(wf.getsampwidth()),
+                             channels=wf.getnchannels(),
+                             rate=wf.getframerate(),
+                             output=True)
+        # Play samples from the wave file (3)
+        while len(data := wf.readframes(1024)) and self.audio_playing is True:
+            stream.write(data)
+        # Close stream (4)
+        stream.close()
+        wf.close()
+
 
 def call_backed(arguments):
     cmd = ["./processThroughFxProgram"]
@@ -89,12 +152,8 @@ def call_backed(arguments):
     try:
         jobj = json.loads(outb.decode("utf-8"))
         return jobj
-    except:
-        return {"error": outb.decode("utf-8")}
-
-
-
-
+    except json.JSONDecodeError:
+        return json.loads('{"error": outb.decode("utf-8")}')
 
 
 if __name__ == "__main__":
