b7e9d8d57ef97bffb7f37e432a83964b43776270
Stonerose35 <fuerholz@gmx.ch>
2024-03-03 09:36:18 +0100

    trying corrected bias issues in waveshaper

--- Src/common/audio/waveShaper.c
+++ Src/common/audio/waveShaper.c
@@ -269,7 +269,7 @@ float multiWaveShaperProcessSample(float sampleIn,MultiWaveShaperDataType*data)
 {
     int32_t indx;
     float v1,v2,rem,factor,indexfloat;
-    indexfloat = ((sampleIn+1.0f)*(float)(WAVESHAPER_CURVE_LENGTH/2));
+    indexfloat = ((sampleIn+1.0f)*((float)((WAVESHAPER_CURVE_LENGTH-1)/2)+0.01f));
     indx = (int32_t)indexfloat; 
     if (indx > (WAVESHAPER_CURVE_LENGTH-1))
     {
@@ -291,7 +291,7 @@ float waveShaperProcessSample(float sampleIn,WaveShaperDataType*data)
 {
     int32_t indx;
     float v1,v2,rem,factor,indexfloat;
-    indexfloat = ((sampleIn+1.0f)*64.0f);
+    indexfloat = ((sampleIn+1.0f)*63.501f);
     indx = (int32_t)indexfloat; // rescale to 0-127
     if (indx > 127)
     {
