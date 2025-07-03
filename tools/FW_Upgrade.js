46e36bb5cec070acdf7a478d2f3b9ab0d514612b
Stonerose35 <fuerholz@gmx.ch>
2025-07-03 23:23:43 +0200

    dfu update web app working

--- tools/FW_Upgrade.js
+++ tools/FW_Upgrade.js
@@ -6,7 +6,7 @@ let firmwareData;
 
 const SET_INTERFACE = 11;
 const DFU_GETSTATUS = 3;
-const DFU_DNLOAD = 2;
+const DFU_DNLOAD = 1;
 const SETUP_REQUEST_DFU_DETACH = 0;
 function init() {
     detectDeviceConsole = document.getElementById("fwu-console-1");
@@ -43,6 +43,7 @@ function fakeUpdatefirmware()
 async function firmwareUpgradeStep()
 {
     let dfuStatus;
+    let outData=new ArrayBuffer(0);
     switch (firmwareUpgradeState.state)
     {
         case "initial":
@@ -82,7 +83,7 @@ async function firmwareUpgradeStep()
             await firmwareUpgradeState.device.claimInterface(2);
             firmwareUpgradeState.stateNr += 1;
             mainConsole.innerText += "\nSetting Interface 2";
-            let outData=new ArrayBuffer(0);
+            
             await firmwareUpgradeState.device.controlTransferOut({requestType: "standard", 
                 recipient: "device",
                 request: SET_INTERFACE,
@@ -157,10 +158,10 @@ async function firmwareUpgradeStep()
             if (dfuStatus.bStatus === 'OK' && dfuStatus.bState === 'IDLE')
             {
                 const fwData =  await fwPromise;
-                const totalBytes = fwData.byteLength;
+                const totalBytes = fwData.byteLength - 16; // TODO_GENERALIZE remove suffix from total size
                 let bytesTransferred=0;
                 let blockNum=0;
-                const TransferSize = 0x40; // so far blindly assuming 64 bytes
+                const TransferSize = 0x40; // TODO_GENERALIZE so far blindly assuming 64 bytes
                 let progress=0;
                 mainConsole.innerText += "\nStarting Download\n";
                 while (bytesTransferred < totalBytes)
@@ -181,13 +182,21 @@ async function firmwareUpgradeStep()
                     request: DFU_DNLOAD,
                     value: blockNum,
                     index: 0},dchunk);
-                    let dlStatus = await getDFUStatus(0);
-                    if (dlStatus.bStatus !== 'OK')
+                    while(true)
                     {
-                        mainConsole.innerText += "\nDownload failed!";
-                        throw new Error("Firmware Download failed");
-                    } 
-                
+                        let dlStatus = await getDFUStatus(0);
+                        if (dlStatus.bStatus !== 'OK')
+                        {
+                            mainConsole.innerText += "\nDownload failed!";
+                            throw new Error("Firmware Download failed");
+                        }
+                        
+                        if (dlStatus.bState === 'DNLOAD_IDLE')
+                        {
+                            break;
+                        }
+                        await new Promise(r => setTimeout(r,20));
+                    }
                     bytesTransferred += bytesToSend;
                     blockNum += 1;
                     if (Math.floor((bytesTransferred/totalBytes)*32.0)> progress)
@@ -200,7 +209,7 @@ async function firmwareUpgradeStep()
                     recipient: "interface",
                     request: DFU_DNLOAD,
                     value: blockNum,
-                    index: 0},new Uint8Array());
+                    index: 0},outData);
                 let dlStatus = await getDFUStatus(0);  
                 if (dlStatus.bStatus !== 'OK')
                 {
@@ -208,12 +217,21 @@ async function firmwareUpgradeStep()
                     throw new Error("Firmware Download failed");
                 } 
                 mainConsole.innerText += "\nDownload succeeded";
+                await new Promise(r => setTimeout(r,20));
                 dlStatus = await getDFUStatus(0);  
                 while (dlStatus.bState !== 'MANIFEST_WAIT_RESET')
                 {
+                    await new Promise(r => setTimeout(r,20));
                     dlStatus = await getDFUStatus(0); 
                 }
                 mainConsole.innerText += "\nFirmware updated successfully";
+                try 
+                {
+                    await firmwareUpgradeState.device.reset();
+                } catch (err)
+                {
+                    console.log(err);
+                }
             }
             break;
 
@@ -353,9 +371,10 @@ function requestDevice()
         firmwareUpgradeState.state = "deviceDetected";
         firmwareUpgradeState.stateNr = 1;
         firmwareUpgradeState.device = usbDevice;
-        mainConsole.innerText += "\nPiPicoFX detected";
+        mainConsole.innerText = "\nPiPicoFX detected";
   })
     .catch(() => {
         detectDeviceConsole.innerText = "No Device Present";
   });
 }
+
