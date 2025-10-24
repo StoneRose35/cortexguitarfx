9c89def7f4b6d26137aa8a30c2ef8bb317e32fff
Stonerose35 <fuerholz@gmx.ch>
2025-10-24 16:29:24 +0200

    readded a apparently necessary header

--- Src/pipicofx/ui/uiLevel0_programchange.cpp
+++ Src/pipicofx/ui/uiLevel0_programchange.cpp
@@ -1,6 +1,7 @@
 extern "C" {
 #include "stdlib.h"
 #include "graphics/bwgraphics.h"
+#include "drivers/display128x64.h"
 #include "drivers/adc.h"
 #include "pipicofx/pipicofxui.h"
 #include "images/pipicofx_param_2_scaled.h"
