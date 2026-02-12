bf88b80cc048f5bbde962199488218f4deca9cde
Stonerose35 <fuerholz@gmx.ch>
2026-02-13 00:00:25 +0100

    added image assets

--- Inc/graphics/bwgraphics.h
+++ Inc/graphics/bwgraphics.h
@@ -2,8 +2,10 @@
 #define _BWGRAPHICS_H_
 #include <stdint.h>
 #include "graphics/gfxfont.h"
+#include "memoryRegions.h"
 #ifndef RP2040_FEATHER
 #include "math.h"
+
 float fsqrt(float a);
 int32_t float2int(float a);
 float int2float(int32_t a);
@@ -14,6 +16,7 @@ float fsin(float x);
 #ifndef __QSPI_CODE
 #define __QSPI_CODE
 #endif
+
 #define BWIMAGE_BW_IMAGE_STRUCT_VERTICAL_BYTES 0
 #define BWIMAGE_BW_IMAGE_STRUCT_HORIZONTAL_BYTES 1
 
@@ -47,28 +50,55 @@ typedef struct BwImageBufferStruct
 } BwImageBufferType;
 
 
+__QSPI_CODE
 uint8_t getPixel(int32_t px,int32_t py,const BwImageTypeConst*img);
+__QSPI_CODE
 void drawLineFloat(float spx,float spy,float epx, float epy,BwImageType* img);
+__QSPI_CODE
 void clearLineFloat(float spx,float spy,float epx, float epy,BwImageType* img);
+__QSPI_CODE
 void setPixel(int32_t px,int32_t py,BwImageType*img);
+__QSPI_CODE
 void clearPixel(int32_t px,int32_t py,BwImageType*img);
 
+__QSPI_CODE
 void drawOval(float ax,float ay,float cx,float cy,BwImageType*img);
+__QSPI_CODE
 void clearOval(float ax,float ay,float cx,float cy,BwImageType*img);
+__QSPI_CODE
 void clearSquare(float spx, float spy,float epx, float epy,BwImageType* img);
+__QSPI_CODE
 void drawSquare(float spx, float spy,float epx, float epy,BwImageType* img);
+__QSPI_CODE
 void clearSquareInt(uint8_t spx, uint8_t spy,uint8_t epx, uint8_t epy,BwImageType* img);
+__QSPI_CODE
 void drawSquareInt(uint8_t spx, uint8_t spy,uint8_t epx, uint8_t epy,BwImageType* img);
+__QSPI_CODE
 void drawHorizontal(uint8_t yval,int8_t sx, int8_t ex, BwImageType*img);
+__QSPI_CODE
 void clearHorizontal(uint8_t yval,int8_t sx, int8_t ex, BwImageType*img);
+__QSPI_CODE
 void drawVertical(uint8_t xval,int8_t sy, int8_t ey, BwImageType*img);
+__QSPI_CODE
 void clearVertical(uint8_t xval,int8_t sy, int8_t ey, BwImageType*img);
+__QSPI_CODE
 void drawLine(uint8_t xstart, uint8_t ystart,uint8_t xend, uint8_t yend,BwImageType*img);
+__QSPI_CODE
 void clearLine(uint8_t xstart,uint8_t xend, uint8_t ystart, uint8_t yend,BwImageType*img);
+__QSPI_CODE
 uint8_t drawChar(uint8_t px, uint8_t py, char c,BwImageType* img,const void* font);
+__QSPI_CODE
 uint8_t drawCharGFXFont(uint8_t px, uint8_t py, char c,BwImageType* img,const GFXfont* font);
+__QSPI_CODE
 uint8_t drawCharOLedFont(uint8_t px, uint8_t py,char c, BwImageType* img);
+__QSPI_CODE
 void drawText(uint8_t px, uint8_t py,const char * txt,BwImageType* img,const void* font);
+__QSPI_CODE
 void drawImage(uint8_t px, uint8_t py,const BwImageTypeConst * img, BwImageType* imgBuffer);
+__QSPI_CODE
 void clearImage(BwImageType*img);
+__QSPI_CODE
+void drawRectFrame(uint8_t xstart,uint8_t ystart,uint8_t xend,uint8_t yend,BwImageType*img);
+__QSPI_CODE
+void clearRectFrame(uint8_t xstart,uint8_t ystart,uint8_t xend,uint8_t yend,BwImageType*img);
 #endif
